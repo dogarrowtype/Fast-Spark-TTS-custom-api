d5f870346bd9d623fe4b106e72e6867a038a5e62
huanghuidmml <m13021933043@163.com>
2025-03-20 17:56:57 +0800

    修复speed_test.py

--- speed_test.py
+++ speed_test.py
@@ -5,21 +5,18 @@
 from typing import Literal
 from fast_sparktts import AsyncFastSparkTTS
 import asyncio
-from time import time
+import time
 
 
 async def run(
         model_path: str,
         num_infer: int = 5,
-        engine: Literal["vllm", "llama-cpp", "sglang"] = "llama-cpp",
+        engine: Literal["vllm", "llama-cpp", "sglang", "torch"] = "torch",
+        device: Literal["cpu", "cuda", "auto"] | str = "auto",
         warmup: bool = False
 ):
     if warmup:
         num_infer = num_infer + 1
-    if engine == "llama-cpp":
-        device = 'cpu'
-    else:
-        device = 'cuda'
 
     model_kwargs = {
         "model_path": model_path,
@@ -29,37 +26,38 @@ async def run(
         "vocoder_device": device,
         "engine": engine
     }
-    if engine in ["vllm", "sglang"]:
+    if engine in ["vllm", "sglang", "torch"]:
         model_kwargs["wav2vec_attn_implementation"] = "sdpa"
         model_kwargs["llm_gpu_memory_utilization"] = 0.6
-
+    if engine == "torch":
+        model_kwargs["torch_dtype"] = "bfloat16"
+        model_kwargs["llm_attn_implementation"] = 'sdpa'
     model = AsyncFastSparkTTS(**model_kwargs)
 
     target_text = "身临其境，换新体验。塑造开源语音合成新范式，让智能语音更自然。"
-    reference_text = "吃燕窝就选燕之屋，本节目由26年专注高品质燕窝的燕之屋冠名播出。豆奶牛奶换着喝，营养更均衡，本节目由豆本豆豆奶特约播出。"
     reference_wav = "data/roles/赞助商/reference_audio.wav"
 
-    t1 = time()
+    start_time = time.perf_counter()
     for i in range(num_infer):
         audio = await model.async_clone_voice(
-            reference_wav_path=reference_wav,
-            reference_text=reference_text,
-            target_text=target_text,
-            temperature=0.2,
+            text=target_text,
+            reference_audio=reference_wav,
+            reference_text=None,
+            temperature=0.6,
             top_p=0.95,
             top_k=40,
-            max_tokens=1024
+            max_tokens=2048
         )
         if warmup and i == 0:
-            t1 = time()
-    t2 = time()
+            start_time = time.perf_counter()
+    end_time = time.perf_counter()
     if warmup:
         num_infer = num_infer - 1
 
     import soundfile as sf
     sf.write("result.wav", audio, 16000, "PCM_16")
-    print(f"inference time: {t2 - t1}")
-    print(f"Average time: {(t2 - t1) / num_infer}")
+    print(f"inference time: {end_time - start_time}")
+    print(f"Average time: {(end_time - start_time) / num_infer}")
 
 
 if __name__ == '__main__':
