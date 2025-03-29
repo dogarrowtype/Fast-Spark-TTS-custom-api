935823ba39ef34331a120fc1a0fb77e124d92dd9
huanghuidmml <m13021933043@163.com>
2025-03-29 22:55:58 +0800

    empty cache

--- fast_tts/engine/spark_engine.py
+++ fast_tts/engine/spark_engine.py
@@ -305,7 +305,8 @@ class AsyncSparkEngine(BaseEngine):
         audio = await self.audio_detokenizer.detokenize_async(
             request=detokenizer_req
         )
-        audio = audio["audio"][0].cpu().numpy().astype(np.float32)
+        audio = audio["audio"][0].detach().cpu().numpy().astype(np.float32)
+        torch.cuda.empty_cache()
         return audio
 
     async def _clone_voice_by_tokens(
